9c830501652cbddd986566a7e40cde2361b63fa9
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-04-03 19:52:32 -0700

    Create a default constructor for ChatServer

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 public class ChatServer {
+    private ServerSocket serverSocket;
+    private ArrayList<PrintWriter> sendList;
+
+    public ChatServer() {
+        // Create a new Server Socket on port 8080 to listen for clients
+        try {
+            this.serverSocket = new ServerSocket(8080);
+        }
+        catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
 }
