4e63444c2ed36a38a3b4870ffbdce7d153cc7c6e
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-04-03 20:11:43 -0700

    Edit ClientHandler.java

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,31 @@
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ClientHandler implements Runnable {
+    private ChatServer chatServer;
+    private BufferedReader clientReader;
+
+    public ClientHandler(ChatServer chatServer, BufferedReader clientReader) {
+        this.chatServer = chatServer;
+        this.clientReader = clientReader;
+    }
+
+    @Override
+    public void run() {
+        watchClientInput();
+    }
+
+    private void watchClientInput() {
+        String message;
+
+        try {
+            while ((message = this.clientReader.readLine()) != null) {
+                System.out.println("Received: " + message + "\n... Sending ..." );
+                this.chatServer.sendToEveryone(message);
+            }
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+
+    }
 }
